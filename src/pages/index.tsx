b1762c16b7eb7a43ad9bd853d248e07bcc155ec1
Joao Silva <joao.devweb@gmail.com>
2022-08-03 20:51:01 -0300

    Implementacoes iniciais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+import common from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,9 +27,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+
+      <Header />
+
+      <main className={`${common.content} ${styles.post}`}>
+        <a>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div>
+            <time>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </time>
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+
+        <a>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div>
+            <time>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </time>
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+          </div>
+        </a>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
