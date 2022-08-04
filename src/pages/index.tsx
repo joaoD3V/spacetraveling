017fc12aa8346be0dde927d45662c7013fc876f2
Joao Silva <joao.devweb@gmail.com>
2022-08-04 12:26:41 -0300

    Finalizacao da Home e inicio do Slug Post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useState } from 'react';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import Header from '../components/Header';
 
@@ -27,7 +31,37 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState<string | null>(
+    postsPagination.next_page
+  );
+
+  function handleLoadMorePosts(url: string) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const loadPosts: Post[] = data.results.map(post => {
+          return {
+            uid: post.uid,
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author,
+            },
+            first_publication_date: format(
+              new Date(post.first_publication_date),
+              'dd LLL yyyy',
+              { locale: ptBR }
+            ),
+          };
+        });
+
+        setPosts(oldPosts => [...oldPosts, ...loadPosts]);
+        setNextPage(data.next_page);
+      });
+  }
+
   return (
     <>
       <Head>
@@ -37,39 +71,67 @@ export default function Home() {
       <Header />
 
       <main className={`${common.content} ${styles.post}`}>
-        <a>
-          <strong>Como utilizar Hooks</strong>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <time>
-              <FiCalendar size={20} /> 15 Mar 2021
-            </time>
-            <span>
-              <FiUser size={20} /> Joseph Oliveira
-            </span>
-          </div>
-        </a>
+        {posts.map(post => (
+          <Link href={`/post/${post.uid}`} key={post.uid}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <time>
+                  <FiCalendar size={20} /> {post.first_publication_date}
+                </time>
+                <span>
+                  <FiUser size={20} /> {post.data.author}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
 
-        <a>
-          <strong>Como utilizar Hooks</strong>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <time>
-              <FiCalendar size={20} /> 15 Mar 2021
-            </time>
-            <span>
-              <FiUser size={20} /> Joseph Oliveira
-            </span>
-          </div>
-        </a>
+        {nextPage && (
+          <button type="button" onClick={() => handleLoadMorePosts(nextPage)}>
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    orderings: {
+      field: 'document.first_publication_date',
+      direction: 'desc',
+    },
+    pageSize: 5,
+  });
 
-//   // TODO
-// };
+  const posts: Post[] = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd LLL yyyy',
+        { locale: ptBR }
+      ),
+    };
+  });
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
